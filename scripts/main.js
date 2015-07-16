b822916981fb856af2cb60c4cfda65fe37a7c003
34C603789a Sam Smale <sam.smale@me.com>
2015-07-16 11:58:21 -0400

    all game logic and card imgs working

--- scripts/main.js
+++ scripts/main.js
@@ -6,266 +6,320 @@ var newDeck ={
     "value": 2,
     "name": "2",
     "suit": "spade",
+    "imgUrl": "assets/PNG-cards-1.3/2_of_spades.png",
   },
   "1": {
     "value": 3,
     "name": "3",
     "suit": "spade",
+    "imgUrl": "assets/PNG-cards-1.3/3_of_spades.png",
   },
   "2": {
     "value": 4,
     "name": "4",
     "suit": "spade",
+    "imgUrl": "assets/PNG-cards-1.3/4_of_spades.png",
   },
   "3": {
     "value": 5,
     "name": "5",
     "suit": "spade",
+    "imgUrl": "assets/PNG-cards-1.3/5_of_spades.png",
   },
   "4": {
     "value": 6,
     "name": "6",
     "suit": "spade",
+    "imgUrl": "assets/PNG-cards-1.3/6_of_spades.png",
   },
   "5": {
     "value": 7,
     "name": "7",
     "suit": "spade",
+    "imgUrl": "assets/PNG-cards-1.3/7_of_spades.png",
   },
   "6": {
     "value": 8,
     "name": "8",
     "suit": "spade",
+    "imgUrl": "assets/PNG-cards-1.3/8_of_spades.png",
   },
   "7": {
     "value": 9,
     "name": "9",
     "suit": "spade",
+    "imgUrl": "assets/PNG-cards-1.3/9_of_spades.png",
   },
   "8": {
     "value": 10,
     "name": "10",
     "suit": "spade",
+    "imgUrl": "assets/PNG-cards-1.3/10_of_spades.png",
   },
   "9": {
     "value": 10,
-    "name": "J",
+    "name": "jack",
     "suit": "spade",
+    "imgUrl": "assets/PNG-cards-1.3/jack_of_spades.png",
   },
   "10": {
     "value": 10,
-    "name": "Q",
+    "name": "queen",
     "suit": "spade",
+    "imgUrl": "assets/PNG-cards-1.3/queen_of_spades.png",
   },
   "11": {
     "value": 10,
-    "name": "K",
+    "name": "king",
     "suit": "spade",
+    "imgUrl": "assets/PNG-cards-1.3/king_of_spades.png",
   },
   "12": {
     "value": 11,
-    "name": "A",
+    "name": "ace",
     "suit": "spade",
+    "imgUrl": "assets/PNG-cards-1.3/ace_of_spades.png",
   },
   "13": {
     "value": 2,
     "name": "2",
     "suit": "clubs",
+    "imgUrl": "assets/PNG-cards-1.3/2_of_clubs.png",
   },
   "14": {
     "value": 3,
     "name": "3",
     "suit": "clubs",
+    "imgUrl": "assets/PNG-cards-1.3/3_of_clubs.png",
   },
   "15": {
     "value": 4,
     "name": "4",
     "suit": "clubs",
+    "imgUrl": "assets/PNG-cards-1.3/4_of_clubs.png",
   },
   "16": {
     "value": 5,
     "name": "5",
     "suit": "clubs",
+    "imgUrl": "assets/PNG-cards-1.3/5_of_clubs.png",
   },
   "17": {
     "value": 6,
     "name": "6",
     "suit": "clubs",
+    "imgUrl": "assets/PNG-cards-1.3/6_of_clubs.png",
   },
   "18": {
     "value": 7,
     "name": "7",
     "suit": "clubs",
+    "imgUrl": "assets/PNG-cards-1.3/7_of_clubs.png",
   },
   "19": {
     "value": 8,
     "name": "8",
     "suit": "clubs",
+    "imgUrl": "assets/PNG-cards-1.3/8_of_clubs.png",
   },
   "20": {
     "value": 9,
     "name": "9",
     "suit": "clubs",
+    "imgUrl": "assets/PNG-cards-1.3/9_of_clubs.png",
   },
   "21": {
     "value": 10,
     "name": "10",
     "suit": "clubs",
+    "imgUrl": "assets/PNG-cards-1.3/10_of_clubs.png",
   },
   "22": {
     "value": 10,
-    "name": "J",
+    "name": "jack",
     "suit": "clubs",
+    "imgUrl": "assets/PNG-cards-1.3/jack_of_clubs.png",
   },
   "23": {
     "value": 10,
-    "name": "Q",
+    "name": "queen",
     "suit": "clubs",
+    "imgUrl": "assets/PNG-cards-1.3/queen_of_clubs.png",
   },
   "24": {
     "value": 10,
-    "name": "K",
+    "name": "king",
     "suit": "clubs",
+    "imgUrl": "assets/PNG-cards-1.3/king_of_clubs.png",
   },
   "25": {
     "value": 11,
-    "name": "A",
+    "name": "ace",
     "suit": "clubs",
+    "imgUrl": "assets/PNG-cards-1.3/ace_of_clubs.png",
   },
   "26": {
     "value": 2,
     "name": "2",
     "suit": "diamonds",
+    "imgUrl": "assets/PNG-cards-1.3/2_of_diamonds.png",
   },
   "27": {
     "value": 3,
     "name": "3",
     "suit": "diamonds",
+    "imgUrl": "assets/PNG-cards-1.3/3_of_diamonds.png",
   },
   "28": {
     "value": 4,
     "name": "4",
     "suit": "diamonds",
+    "imgUrl": "assets/PNG-cards-1.3/4_of_diamonds.png",
   },
   "29": {
     "value": 5,
     "name": "5",
     "suit": "diamonds",
+    "imgUrl": "assets/PNG-cards-1.3/5_of_diamonds.png",
   },
   "30": {
     "value": 6,
     "name": "6",
     "suit": "diamonds",
+    "imgUrl": "assets/PNG-cards-1.3/6_of_diamonds.png",
   },
   "31": {
     "value": 7,
     "name": "7",
     "suit": "diamonds",
+    "imgUrl": "assets/PNG-cards-1.3/7_of_diamonds.png",
   },
   "32": {
     "value": 8,
     "name": "8",
     "suit": "diamonds",
+    "imgUrl": "assets/PNG-cards-1.3/8_of_diamonds.png",
   },
   "33": {
     "value": 9,
     "name": "9",
     "suit": "diamonds",
+    "imgUrl": "assets/PNG-cards-1.3/9_of_diamonds.png",
   },
   "34": {
     "value": 10,
     "name": "10",
     "suit": "diamonds",
+    "imgUrl": "assets/PNG-cards-1.3/10_of_diamonds.png",
   },
   "35": {
     "value": 10,
-    "name": "J",
+    "name": "jack",
     "suit": "diamonds",
+    "imgUrl": "assets/PNG-cards-1.3/jack_of_diamonds.png",
   },
   "36": {
     "value": 10,
-    "name": "Q",
+    "name": "queen",
     "suit": "diamonds",
+    "imgUrl": "assets/PNG-cards-1.3/queen_of_diamonds.png",
   },
   "37": {
     "value": 10,
-    "name": "K",
+    "name": "king",
     "suit": "diamonds",
+    "imgUrl": "assets/PNG-cards-1.3/king_of_diamonds.png",
   },
   "38": {
     "value": 11,
-    "name": "A",
+    "name": "ace",
     "suit": "diamonds",
+    "imgUrl": "assets/PNG-cards-1.3/ace_of_diamonds.png",
   },
   "39": {
     "value": 2,
     "name": "2",
     "suit": "hearts",
+    "imgUrl": "assets/PNG-cards-1.3/2_of_hearts.png",
   },
   "40": {
     "value": 3,
     "name": "3",
     "suit": "hearts",
+    "imgUrl": "assets/PNG-cards-1.3/3_of_hearts.png",
   },
   "41": {
     "value": 4,
     "name": "4",
     "suit": "hearts",
+    "imgUrl": "assets/PNG-cards-1.3/4_of_hearts.png",
   },
   "42": {
     "value": 5,
     "name": "5",
     "suit": "hearts",
+    "imgUrl": "assets/PNG-cards-1.3/5_of_hearts.png",
   },
   "43": {
     "value": 6,
     "name": "6",
     "suit": "hearts",
+    "imgUrl": "assets/PNG-cards-1.3/6_of_hearts.png",
   },
   "44": {
     "value": 7,
     "name": "7",
     "suit": "hearts",
+    "imgUrl": "assets/PNG-cards-1.3/7_of_hearts.png",
   },
   "45": {
     "value": 8,
     "name": "8",
     "suit": "hearts",
+    "imgUrl": "assets/PNG-cards-1.3/8_of_hearts.png",
   },
   "46": {
     "value": 9,
     "name": "9",
     "suit": "hearts",
+    "imgUrl": "assets/PNG-cards-1.3/9_of_hearts.png",
   },
   "47": {
     "value": 10,
     "name": "10",
     "suit": "hearts",
+    "imgUrl": "assets/PNG-cards-1.3/10_of_hearts.png",
   },
   "48": {
     "value": 10,
-    "name": "J",
+    "name": "jack",
     "suit": "hearts",
+    "imgUrl": "assets/PNG-cards-1.3/jack_of_hearts.png",
   },
   "49": {
     "value": 10,
-    "name": "Q",
-    "suit": "hearcats",
+    "name": "queen",
+    "suit": "hearts",
+    "imgUrl": "assets/PNG-cards-1.3/queen_of_hearts.png",
   },
   "50": {
     "value": 10,
-    "name": "K",
+    "name": "king",
     "suit": "hearts",
+    "imgUrl": "assets/PNG-cards-1.3/king_of_hearts.png",
   },
   "51": {
     "value": 11,
-    "name": "A",
+    "name": "ace",
     "suit": "hearts",
+    "imgUrl": "assets/PNG-cards-1.3/ace_of_hearts.png",
   },
 
 }
 var dealerCardValue = [];
 var playerCardValue = [];
+var dealerHand = [];
+var playerHand = [];
 var cardsInPlay = [];
 var bank = 1000;
 // var gameState = true;
@@ -276,8 +330,10 @@ getPlayerCard = function (){
 	var card = Math.floor(Math.random() * 52);
 	var duplicateCheck = checkForDuplicates(card);
 	if (duplicateCheck === false) {
-		playerCardValue.push(newDeck[card].value)
-		cardsInPlay.push(card)
+		playerCardValue.push(newDeck[card].value);
+		cardsInPlay.push(card);
+		playerHand.push(card);
+		$('.playerCards').prepend($('<img>', {class:'card',src: newDeck[card].imgUrl}))
 	} else getPlayerCard();
 }
 
@@ -285,9 +341,23 @@ getDealerCard = function (){
 	var card = Math.floor(Math.random() * 52);
 	var duplicateCheck = checkForDuplicates(card);
 	if (duplicateCheck === false) {
-		dealerCardValue.push(newDeck[card].value)
-		cardsInPlay.push(card)
+		dealerCardValue.push(newDeck[card].value);
+		cardsInPlay.push(card);
+		dealerHand.push(card);
+		$('.dealerCards').prepend($('<img>', {class:'hidden card',src: newDeck[card].imgUrl}))
 	} else getDealerCard();
+}
+
+checkForAces = function(element){
+	if ($.inArray(12, element) > 0) {
+		return true;
+	} else if ($.inArray(25, element) > 0) {
+		return true;
+	} else if ($.inArray(38, element) > 0) {
+		return true;
+	} else if ($.inArray(51, element) > 0) {
+		return true;
+	} else return false;
 }
 
 checkForDuplicates = function(element){
@@ -303,12 +373,20 @@ playerValue = function(){
 	for (var i = 0; i < playerCardValue.length; i++) {
     total += playerCardValue[i] << 0;
 	}
-	$('.playerValue').text(total);
-	if (total > 21) {
-		console.log('You lose!');
+	console.log(checkForAces(playerHand));
+	console.log(playerHand)
+	if (total > 21 && checkForAces(playerHand) === true) {
+		total = total - 10;
+		$('.playerValue').text(total);
+	} else if (total > 21) {
+		showCards();
+		$('.outcome').text('You lose!');
+		console.log('Bust! You lose!');
 		bank = parseInt(bank) - parseInt($('.bet').val());
 		$('.bank').text(bank);
-	}
+		$('.playerValue').text(total);
+	} else
+	$('.playerValue').text(total);
 	return total;
 	}
 
@@ -336,12 +414,21 @@ dealerLogic = function (){
 	// gameState = false
 }
 
+showCards = function(){
+	$('.hidden').removeClass('hidden')
+
+}
+
 dealerValue = function(){
 	var total = 0;
 	for (var i = 0; i < dealerCardValue.length; i++) {
     total += dealerCardValue[i] << 0;
-	}
-	$('.dealerValue').text(total)
+    console.log(checkForAces(dealerHand));
+    console.log(dealerHand)
+	}if (total > 21 && checkForAces(dealerHand) === true) {
+		total = total - 10;
+		$('.dealerValue').text(total);
+	} else $('.dealerValue').text(total)
 	return total;
 }
 
@@ -350,6 +437,7 @@ gameOver = function(){
 	console.log(playerValue())
 	console.log(dealerValue())
 	console.log($('.bet').val())
+	showCards();
 	if (playerValue() === 21 && dealerValue() != 21) {
 		$('.outcome').text('21! You win!');
 		console.log('21! You win!');
@@ -358,7 +446,7 @@ gameOver = function(){
 		$('.outcome').text('Bust! You lose!');
 		console.log('Bust! You lose!');
 		bank = parseInt(bank) - parseInt($('.bet').val())
-} else if (dealerValue > 17 && playerValue() === dealerValue()){
+} else if (playerValue() === dealerValue()){
 		$('.outcome').text('Push!');
 		console.log('Push!');
 		bank = parseInt(bank) + parseInt($('.bet').val());
@@ -382,8 +470,14 @@ deal = function (){
 	bank = parseInt($('.bank').text()) - parseInt($('.bet').val());
 	$('.bank').text(bank);
 	$('.outcome').text('');
+	$('.dealerValue').addClass('hidden');
+	$('.dealerCards').addClass('hidden');
+	$('.playerCards img').remove();
+	$('.dealerCards img').remove();
 	dealerCardValue = [];
 	playerCardValue = [];
+	playerHand = [];
+	dealerHand = [];
 	cardsInPlay = [];
 
 	getPlayerCard();
